8a1c2c48b325d109a0f98aba3bd0dc175bfb5e1c
royger <royger@gmail.com>
2010-11-23 11:30:18 +0100

    Split mutex limiter into two separate mutexes, one for upload and one for download

--- Limiter/Limiter.go
+++ Limiter/Limiter.go
@@ -13,7 +13,8 @@ const(
 
 type limiter struct {
 	reset *time.Ticker
-	mutex *sync.Mutex
+	up_mutex *sync.Mutex
+	down_mutex *sync.Mutex
 	upload, download, up_reset, down_reset, wait_upload, wait_download int64
 	up_chan, down_chan chan bool
 }
@@ -27,13 +28,14 @@ func NewLimiter(up_limit, down_limit int) (Limiter, os.Error) {
 	l := new(limiter)
 	l.upload, l.up_reset, l.download, l.down_reset = -1, -1, -1, -1
 	if up_limit > 0 || down_limit > 0 {
-		l.mutex = new(sync.Mutex)
 		l.reset = time.NewTicker(NS_PER_S)
 		if up_limit > 0 {
+			l.up_mutex = new(sync.Mutex)
 			l.up_chan = make(chan bool)
 			l.upload, l.up_reset = int64(up_limit)*1000, int64(up_limit)*1000
 		}
 		if down_limit > 0 {
+			l.down_mutex = new(sync.Mutex)
 			l.down_chan = make(chan bool)
 			l.download, l.down_reset = int64(down_limit)*1000, int64(down_limit)*1000
 		}
@@ -44,13 +46,13 @@ func NewLimiter(up_limit, down_limit int) (Limiter, os.Error) {
 
 func (l *limiter) WaitSend(size int64) int64 {
 	if l.upload != -1 {
-		l.mutex.Lock()
-		defer l.mutex.Unlock()
+		l.up_mutex.Lock()
+		defer l.up_mutex.Unlock()
 		for(l.upload == 0) {
 			l.wait_upload++
-			l.mutex.Unlock()
+			l.up_mutex.Unlock()
 			<- l.up_chan
-			l.mutex.Lock()
+			l.up_mutex.Lock()
 		}
 		left := l.upload - size
 		if left < 0 {
@@ -66,13 +68,13 @@ func (l *limiter) WaitSend(size int64) int64 {
 
 func (l *limiter) WaitReceive(size int64) int64 {
 	if l.download != -1 {
-		l.mutex.Lock()
-		defer l.mutex.Unlock()
+		l.down_mutex.Lock()
+		defer l.down_mutex.Unlock()
 		for(l.download == 0) {
 			l.wait_download++
-			l.mutex.Unlock()
+			l.down_mutex.Unlock()
 			<- l.down_chan
-			l.mutex.Lock()
+			l.down_mutex.Lock()
 		}
 		left := l.download - size
 		if left < 0 {
@@ -90,13 +92,18 @@ func (l *limiter) run() {
 	for {
 		select {
 		case <- l.reset.C:
-			l.mutex.Lock()
+			// Reset upload limit
+			l.up_mutex.Lock()
 			l.upload = l.up_reset
-			l.download = l.down_reset
 			// Wake up waiting threads
 			for ; l.wait_upload > 0; l.wait_upload-- { l.up_chan <- true }
+			l.up_mutex.Unlock()
+			// Reset download limit
+			l.down_mutex.Lock()
+			l.download = l.down_reset
+			// Wake up waiting threads
 			for ; l.wait_download > 0; l.wait_download-- { l.down_chan <- true }
-			l.mutex.Unlock()
+			l.down_mutex.Unlock()
 		}
 	}
 } 
